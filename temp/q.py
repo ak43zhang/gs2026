01228b59a94f11be6b3e3a7ebdc3fa37f2e50c3b
win10_zq <m17600700886@163.com>
2026-07-25 05:16:53 +0800

    cleanup: 清理temp目录61个临时文件
    删除:
    - 39个Python临时脚本
    - 16个TXT临时文件
    - 2个LOG文件
    - 3个JSON文件
    - 1个BAT文件
    
    保留:
    - 10个SQL数据库变更脚本
    
    temp/目录从71个文件减少到10个

--- temp/q.py
+++
@@ -1,6 +0,0 @@
-import sys
-f=open(r'F:\pyworkspace2026\gs2026\src\gs2026\dashboard2\templates\monitor.html',encoding='utf-8')
-c=f.read()
-f.close()
-count = c.count('bp-tab-btn')
-sys.stdout.write(str(count))
